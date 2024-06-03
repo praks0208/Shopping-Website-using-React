73c0b5c28f44cd81d49ef38ea5c97721c23d3d5a
praks0208 <praks.sawant26@gmail.com>
2024-06-03 21:39:22 +0530

    Migrate the Entire Demo Project to use the Context API

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
- const {items} = useContext(CartContext);
+export default function Cart() {
+ const {items, updateItemQuantity} = useContext(CartContext);
  
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -19,17 +19,19 @@ export default function Cart({ onUpdateItemQuantity }) {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
+              
+
               <li key={item.id}>
                 <div>
                   <span>{item.name}</span>
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
@@ -42,5 +44,6 @@ export default function Cart({ onUpdateItemQuantity }) {
         Cart Total: <strong>{formattedTotalPrice}</strong>
       </p>
     </div>
+
   );
 }
